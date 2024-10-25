a2540d2d0256737b141127fdc3ad17474eef8dd3
adis azhar <adisazhar123@gmail.com>
2024-10-24 22:29:32 -0500

    add methods to build span from internalspan

--- jaeger-storage/model.go
+++ jaeger-storage/model.go
@@ -46,23 +46,94 @@ type InternalSpanRef struct {
 }
 
 type InternalSpan struct {
-	Id          int64         `db:"id"`
-	SpanId      string        `db:"span_id"`
-	TraceId     string        `db:"trace_id"`
-	OperationId int64         `db:"operation_id"`
-	Flags       int64         `db:"flags"`
-	StartTime   time.Time     `db:"start_time"`
-	Duration    time.Duration `db:"duration"`
-	Tags        []byte        `db:"tags"`
-	ServiceId   int64         `db:"service_id"`
-	ProcessId   string        `db:"process_id"`
-	ProcessTags []byte        `db:"process_tags"`
-	Warnings    []string      `db:"warnings"`
-	Logs        []byte        `db:"logs"`
-	Kind        string        `db:"kind"`
-	Refs        []byte        `db:"refs"`
-	CreatedAt   time.Time     `db:"created_at"`
-	DeletedAt   *time.Time    `db:"deleted_at"`
+	Id          int64              `db:"id"`
+	SpanId      string             `db:"span_id"`
+	TraceId     string             `db:"trace_id"`
+	OperationId int64              `db:"operation_id"`
+	Operation   *InternalOperation `db:"operation"`
+	Flags       int64              `db:"flags"`
+	StartTime   time.Time          `db:"start_time"`
+	Duration    time.Duration      `db:"duration"`
+	Tags        []byte             `db:"tags"`
+	ServiceId   int64              `db:"service_id"`
+	Service     *InternalService   `db:"service"`
+	ProcessId   string             `db:"process_id"`
+	ProcessTags []byte             `db:"process_tags"`
+	Warnings    []string           `db:"warnings"`
+	Logs        []byte             `db:"logs"`
+	Kind        string             `db:"kind"`
+	Refs        []byte             `db:"refs"`
+	CreatedAt   time.Time          `db:"created_at"`
+	DeletedAt   *time.Time         `db:"deleted_at"`
+}
+
+func (s InternalSpan) ToSpan() (*model.Span, error) {
+	traceId, err := model.TraceIDFromString(s.TraceId)
+	if err != nil {
+		return nil, err
+	}
+
+	spanId, err := model.SpanIDFromString(s.SpanId)
+	if err != nil {
+		return nil, err
+	}
+
+	var operationName = ""
+	if s.Operation != nil {
+		operationName = s.Operation.Name
+	}
+
+	references, err := decodeReferences(s.Refs)
+	if err != nil {
+		return nil, err
+	}
+
+	tags, err := decodeTags(s.Tags)
+	if err != nil {
+		return nil, err
+	}
+
+	logs, err := decodeLogs(s.Logs)
+	if err != nil {
+		return nil, err
+	}
+
+	process, err := s.getProcess()
+	if err != nil {
+		return nil, err
+	}
+
+	return &model.Span{
+		TraceID:       traceId,
+		SpanID:        spanId,
+		OperationName: operationName,
+		References:    references,
+		Flags:         model.Flags(s.Flags),
+		StartTime:     s.StartTime,
+		Duration:      s.Duration,
+		Tags:          tags,
+		Logs:          logs,
+		Process:       process,
+		ProcessID:     s.ProcessId,
+		Warnings:      s.Warnings,
+	}, nil
+}
+
+func (s InternalSpan) getProcess() (*model.Process, error) {
+	serviceName := ""
+	if s.Service != nil {
+		serviceName = s.Service.Name
+	}
+
+	tags, err := decodeTags(s.ProcessTags)
+	if err != nil {
+		return nil, err
+	}
+
+	return &model.Process{
+		ServiceName: serviceName,
+		Tags:        tags,
+	}, nil
 }
 
 type InternalOperation struct {
